ed6e3aa03a87be12ecd2391067a8ae3e1eda685b
Matthew Fortunka <mafortun@microsoft.com>
2019-11-27 15:41:59 +0000

    added branch

---
+++ testmvc/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using testmvc.Models;
+
+namespace testmvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            
+            var client = _httpClientFactory.CreateClient("testapiclient");
+
+            TempData["baseAddress"] = client.BaseAddress.ToString();
+            var response = await client.GetAsync("/WeatherForecast");
+            if(!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException("Failed to call TestAPI");
+            }
+
+            TempData["forecast"] = await response.Content.ReadAsStringAsync();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
